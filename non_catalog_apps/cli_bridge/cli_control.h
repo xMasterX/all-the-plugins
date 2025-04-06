e89cbf84e9537914c0d1f082077f695f47d580f4
MX <10697207+xMasterX@users.noreply.github.com>
2025-04-06 04:37:12 +0300

    rm cligui from pack for now

--- non_catalog_apps/cli_bridge/cli_control.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <furi.h>
-#include <furi_hal.h>
-extern void clicontrol_hijack(size_t tx_size, size_t rx_size);
-extern void clicontrol_unhijack(bool persist);
-extern FuriStreamBuffer* cli_tx_stream;
-extern FuriStreamBuffer* cli_rx_stream;
