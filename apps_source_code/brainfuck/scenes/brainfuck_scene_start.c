ab3ad14bd119391bdba2c20bdf809d9bcea5e569
Tyler Anderson <130514297+Tyl3rA@users.noreply.github.com>
2026-01-23 23:23:23 -0700

    Update BF app to v1.5

--- apps_source_code/brainfuck/scenes/brainfuck_scene_start.c
+++ apps_source_code/brainfuck/scenes/brainfuck_scene_start.c
@@ -2,6 +2,7 @@
 enum SubmenuIndex {
     SubmenuIndexNew,
     SubmenuIndexOpen,
+    SubmenuIndexLearn,
     SubmenuIndexAbout,
 };
 
@@ -17,6 +18,8 @@ void brainfuck_scene_start_on_enter(void* context) {
         submenu, "New", SubmenuIndexNew, brainfuck_scene_start_submenu_callback, brainfuck);
     submenu_add_item(
         submenu, "Open", SubmenuIndexOpen, brainfuck_scene_start_submenu_callback, brainfuck);
+    submenu_add_item(
+        submenu, "Learn", SubmenuIndexLearn, brainfuck_scene_start_submenu_callback, brainfuck);
     submenu_add_item(
         submenu, "About", SubmenuIndexAbout, brainfuck_scene_start_submenu_callback, brainfuck);
 
@@ -35,6 +38,19 @@ bool brainfuck_scene_start_on_event(void* context, SceneManagerEvent event) {
             consumed = true;
         } else if(event.event == SubmenuIndexOpen) {
             scene_manager_next_scene(brainfuck->scene_manager, brainfuckSceneFileSelect);
+            consumed = true;
+        } else if(event.event == SubmenuIndexLearn) {
+            text_box_set_text(
+                brainfuck->text_box,
+                "The BF program space is comprised of a one dimensional array 128 cells long and is made up of eight commands:\n\n"
+                "'>': Increment the data pointer (to point to the next cell to the right).\n\n"
+                "'<': Decrement the data pointer (to point to the next cell to the left).\n\n"
+                "'+': Increment (increase by one) the byte at the data pointer.\n\n"
+                "'-': Decrement (decrease by one) the byte at the data pointer.\n\n"
+                "'.': Output the byte at the data pointer.\n\n"
+                "',' : Accept one byte of input, storing its value in the byte at the data pointer.\n\n"
+                "'[': If the byte at the data pointer is zero, then instead of moving the instruction pointer forward to the next command, jump it forward to the command after the matching ']'. If it is not zero then the code inside the brackets will loop until it reaches a 0 value.\n");
+            scene_manager_next_scene(brainfuck->scene_manager, brainfuckSceneExecEnv);
             consumed = true;
         } else if(event.event == SubmenuIndexAbout) {
             text_box_set_text(
