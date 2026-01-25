71e51540a193c8fa23d593b5f4f65a35cbfabcd9
MX <10697207+xMasterX@users.noreply.github.com>
2026-01-26 02:45:55 +0300

    upd passy

--- non_catalog_apps/passy/passy_reader.c
+++ non_catalog_apps/passy/passy_reader.c
@@ -497,7 +497,7 @@ NfcCommand passy_reader_state_machine(PassyReader* passy_reader) {
             view_dispatcher_send_custom_event(passy->view_dispatcher, PassyCustomEventReaderError);
             break;
         }
-        FURI_LOG_I(TAG, "Mututal authentication success");
+        FURI_LOG_I(TAG, "Mutual authentication success");
         passy_secure_messaging_calculate_session_keys(passy_reader->secure_messaging);
         use_secure_messaging = true;
         view_dispatcher_send_custom_event(
